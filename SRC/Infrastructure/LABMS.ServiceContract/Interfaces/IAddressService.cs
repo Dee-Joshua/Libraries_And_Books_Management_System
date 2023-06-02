7290b41ec7b620689394e3517f1e2372e1835cd0
Dee-Joshua <officialdariajoshua@gmail.com>
2023-06-02 18:30:54 +0100

    Service Contract Layer with Interfaces added. Awaiting Update to this layer

---
+++ SRC/Infrastructure/LABMS.ServiceContract/Interfaces/IAddressService.cs
@@ -0,0 +1,20 @@
+﻿using LABMS.Application.DTOs.ForCreation;
+using LABMS.Application.DTOs.ForDto;
+using LABMS.Application.DTOs.ForUpdate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABMS.ServiceContract.Interfaces
+{
+    public interface IAddressService
+    {
+        Task<IEnumerable<AddressDto>> GetAllAddressesAsync(bool trackChanges);
+        Task<AddressDto> GetAddressAsync(int id, bool trackChanges);
+        Task<AddressDto> CreateAddressAsync(AddressForCreation addressCreationDto, bool trackChanges);
+        Task UpdateAddressAsync(int id, AddressForUpdate addressUpdateDto, bool trackChanges);
+        Task DeleteAddressAsync(int id, bool trackChanges);
+    }
+}
